0043cb99c31f10648f8d37b3149fa9deac92377e
omogbolahan01 <iloriojommodel@gmail.com>
2023-12-06 00:45:22 +0100

    first

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react-dom";
+export default function Home() {
+  return (
+    <div className="home-region">
+      <div className="top-region">
+        <img src="/images/2410.png" />
+        <h3>
+          We're crafting a space where creativity knows no bounds. 2410 Studio
+          is under construction, and we can't wait to share our passion for
+          design and No-Code development with you.
+        </h3>
+        <div>
+          <p>Have questions or inquiries?</p>
+          <button>Send us an email</button>
+        </div>
+      </div>
+      <div className="empty"></div>
+      <div className="social-area">
+        <h4>Follows us on</h4>
+        <div className="social-media">
+          <img src="/images/facebook.png" />
+          <img src="/images/instagram (2).png" />
+          <img src="/images/linkedin (4).png" />
+          <img src="/images/behance.png" />
+        </div>
+      </div>
+    </div>
+  );
+}
